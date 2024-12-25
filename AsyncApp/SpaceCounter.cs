146034ed1e788222df817fd465db9154461b8cd9
iHeyd <0_iheyd_0@mail.ru>
2024-12-26 00:12:28 +0700

    1.0

--- AsyncApp/SpaceCounter.cs
+++ AsyncApp/SpaceCounter.cs
@@ -2,7 +2,7 @@ namespace AsyncApp
 {
     public static class SpaceCounter
     {
-        public static int CountSpaces(string? content)
+        public static int CountSpaces(string content)
         {
             return content.Count(c => c == ' ');
         }
